13fb04c41f23d68b2c76823018d12631ae2f56ff
Yukaru <nilsrw@gmx.de>
2024-05-14 21:20:21 +0200

    refactor i18n

--- types/i18.ts
+++ types/i18.ts
@@ -1,10 +1,4 @@
 type I18 = (key: string, params?: Record<string, string>, defaultValue?: string) => any;
-
-type I18ProviderValue = [
-    I18,
-    {
-        add: (lang: string, table: Record<string, any>) => void;
-        locale: (lang?: string) => string;
-        dict: (lang: string) => Record<string, Record<string, any>>;
-    }
-];
+type I18Add = (lang: string, table: Record<string, any>) => void;
+type I18Locale = (lang?: string) => string;
+type I18Dict = (lang: string) => Record<string, Record<string, any>>;
